33c1027beca1102b767d4a9c2e70884f54f7372d
Ihor Tkachenko <igortkachenko987@gmail.com>
2024-09-10 10:51:16 +0200

    FirstTests

---
+++ REST_API_TEST/src/test/java/HelloWorldTest.java
@@ -0,0 +1,77 @@
+import io.restassured.RestAssured;
+import io.restassured.http.Headers;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class HelloWorldTest {
+    @Test
+    public void testRestAssured(){
+        Map<String,String> params = new HashMap<>();
+        params.put("name", "John Snow");
+       JsonPath response = RestAssured
+               .given()
+               .queryParams(params)
+               .get("https://playground.learnqa.ru/api/hello")
+               .jsonPath();
+
+       String answer = response.get("answer4");
+       if(answer==null){
+           System.out.println("The key 'answer4' is absent");
+       }else{
+           System.out.println(answer);
+       }
+    }
+
+    @Test
+    public void testRestAssured1(){
+        Map<String, Object> body = new HashMap<>();
+        body.put("param1", "Value1");
+        body.put("param2", "Value2");
+
+        Response response = RestAssured
+                .given()
+                .body(body)
+                .post("https://playground.learnqa.ru/api/check_type")
+                .andReturn();
+        response.print();
+        int statusCode = response.statusCode();
+        System.out.println(statusCode);
+
+    }
+
+    @Test
+    public void testRestAssured2(){
+        Map<String,String> headers = new HashMap<>();
+        headers.put("myHeader1","myValue1");
+        headers.put("myHeader2","myValue2");
+
+        Response response= RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .when()
+                .get("https://playground.learnqa.ru/api/get_303")
+                .andReturn();
+
+        response.prettyPrint();
+
+        String locationHeader = response.getHeader("Location");
+        System.out.println(locationHeader);
+
+        System.out.println("=======================================================");
+
+        Headers responseHeaders = response.getHeaders();
+        System.out.println(responseHeaders);
+
+        System.out.println("=======================================================");
+
+        int statusCode = response.statusCode();
+        System.out.println(statusCode);
+    }
+
+}
