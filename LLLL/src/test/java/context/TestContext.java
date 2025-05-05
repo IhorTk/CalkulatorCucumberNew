8f3c31e686c56c8671284211a4909ef1c06f1b9d
IgorTkachenko <158606988+IhorTk@users.noreply.github.com>
2025-05-05 20:49:13 +0200

    Delete LLLL/src/test/java directory

--- LLLL/src/test/java/context/TestContext.java
+++
@@ -1,9 +0,0 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-public class TestContext {
-    public WebDriver driver;
-    public WebDriverWait wait;
-    public Actions action;
-}
